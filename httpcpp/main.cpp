c836e1562636a3d0e2d7c5237bd5609c79a4ba0b
Joe Bayer <jb71999@gmail.com>
2021-07-22 12:12:45 +0200

    Project rename

--- httpcpp/main.cpp
+++ httpcpp/main.cpp
@@ -1,14 +1,32 @@
 //
 //  main.cpp
-//  httpcpp
+//  simple http server
 //
 //  Created by Joe Bayer on 22/07/2021.
 //
 
 #include <iostream>
+#include "http_server.h"
+
+
+void home(){
+    std::cout << "Hello, World!\n";
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+
+    struct http_config config;
+    config.port = 8080;
+    config.debug = 1;
+    
+    
+    http_server server;
+    
+    server.add_config(config);
+    
+    method_t get_method = GET;
+    
+    server.add_route("/", get_method, &home);
+    
     return 0;
 }
